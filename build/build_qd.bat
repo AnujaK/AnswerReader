f0dd7c0ab82d5dce738332bf45dea7de5e4e2fd2
Ranjan Kumar <ranjan.rk@gmail.com>
2014-04-29 12:39:43 +0530

    removed unused code

--- build/build_qd.bat
+++
@@ -1,8 +0,0 @@
-set TRUNK_LOCATION=D:\chrome_dev\SlashRecruit\quoradeck_017
-set DIST_LOCATION=D:\chrome_dev\quoradeck_dist
-set YUI_COMPRESSOR_LOCATION=D:/chrome_dev/yuicompressor-2.4.2.jar
-
-C:\Windows\System32\xcopy.exe %TRUNK_LOCATION% %DIST_LOCATION%\ /S
-
-java -jar %YUI_COMPRESSOR_LOCATION% --type js %DIST_LOCATION%\src\idxdb.js -o %DIST_LOCATION%\src\idxdb.js
-java -jar %YUI_COMPRESSOR_LOCATION% --type js %DIST_LOCATION%\src\main-view.js -o %DIST_LOCATION%\src\main-view.js
